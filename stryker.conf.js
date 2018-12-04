65805b53b688aaa9fcd4d3c840e56109c161af2c
Emanuele Biancardi <emanuele.biancardi@wyscout.com>
2018-12-04 04:14:27 +0100

    Fix stryker configuration, stryker npm script and add stryker badge to README

--- stryker.conf.js
+++ stryker.conf.js
@@ -2,10 +2,10 @@
 // Please see the handbook for more information: https://github.com/stryker-mutator/stryker-handbook/blob/master/stryker/guides/react.md#react
 module.exports = function(config) {
   config.set({
-    mutate: ["src/**/*.js", "!src/**/*@(.test|.spec|Spec).js"],
+    mutate: ["src/@(actions|reducers)/*.js", "!src/@(actions|reducers)/__tests__/*@(.test|.spec|Spec).js"],
     mutator: "javascript",
     testRunner: "jest",
-    reporters: ["progress", "clear-text", "html"],
+    reporters: ["progress", "clear-text", "html", "dashboard"],
     coverageAnalysis: "off",
     jest: {
       projectType: "react"
